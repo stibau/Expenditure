42f3c8fb0f715ac2e8bbc1fa0c942a8727cbbab7
stibau <48186853+stibau@users.noreply.github.com>
2025-05-24 21:23:01 +0200

    Adding a minimal demo service that returns a couple example expenses (#12)

---
+++ Service.Test/CostRepartitionServiceFixture.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Services;
+using Xunit;
+
+namespace Service.Test;
+
+public class CostRepartitionServiceFixture
+{
+    [Fact]
+    public void GetAllCosts_Should_ReturnData()
+    {
+        // Arranged
+        var sut = new CostRepartitionService();
+
+        // Act
+        var actual = sut.GetAllExpenses();
+
+        // Assert
+        actual.Should().NotBeEmpty();
+    }
+
+    [Theory]
+    [InlineData(3)]
+    public void GetAllCosts_ShouldReturnTheCorrectNumberOfElements(int expectedCount)
+    {
+        // Arrange
+        var sut = new CostRepartitionService();
+
+        // Act
+        var actual = sut.GetAllExpenses();
+
+        // Assert
+        actual.Count.Should().Be(expectedCount);
+    }
+
+    [Theory]
+    [AutoData]
+    public void GetAllCosts_ShouldReturnTheDataAsExpected(CostRepartitionService sut)
+    {
+        // Arrange
+        var expected = new List<Expense>
+        {
+            new("Stef", "ENGIE", 167.55m),
+            new("Euge", "Carrefour", 82.3m),
+            new("Stef", "Carrefour", 50m)
+        };
+
+        // Act
+        var actual = sut.GetAllExpenses();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ Services/CostRepartitionService.cs
@@ -0,0 +1,25 @@
+﻿
+// ReSharper disable NotAccessedPositionalProperty.Global
+namespace Services;
+
+public record Expense(string Initiator, string Purpose, decimal Amount);
+
+public class CostRepartitionService
+{
+    private readonly List<Expense> _expenses;
+
+    public CostRepartitionService()
+    {
+        _expenses = new List<Expense>();
+
+        //Add some initial data for testing purposes
+        _expenses.Add(new Expense("Stef", "ENGIE", 167.55m));
+        _expenses.Add(new Expense("Euge", "Carrefour", 82.3m));
+        _expenses.Add(new Expense("Stef", "Carrefour", 50m));
+    }
+
+    public List<Expense> GetAllExpenses()
+    {
+        return _expenses;
+    }
+}
